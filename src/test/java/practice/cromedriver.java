5d676d9f7299bb00af941a2a4c837a0e0b3d3e18
CHIDIRALASAIKIRANREDDY <157891389+CHIDIRALASAIKIRANREDDY@users.noreply.github.com>
2025-03-18 23:29:34 +0530

    Update cromedriver.java

--- src/test/java/practice/cromedriver.java
+++ src/test/java/practice/cromedriver.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebDriverInfo;
 public class cromedriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
 	}
 
 }
